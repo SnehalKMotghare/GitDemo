a3f1f756a25a04210dfe838b64050282cbce3a9c
snehalkmotghare <Snehal.Motghare@lntinfotech.com>
2022-10-26 21:24:29 +0530

    first commit

---
+++ src/main/java/Maven/SeleniumTest.java
@@ -0,0 +1,34 @@
+package Maven;
+import org.testng.annotations.Test;
+
+public class SeleniumTest
+{
+	
+//	 <plugin>
+//     <artifactId>maven-surefire-plugin</artifactId>
+//     <version>2.22.1</version>
+//   </plugin>
+	
+	
+	@Test
+	public void testSelenium()
+	{
+		System.out.println("testSelenium");
+	}
+	
+	@Test
+	public void testSeleniumForMaven()
+	{
+		System.out.println("testSeleniumForMaven");
+	}
+}
+
+
+//<groupId>org.apache.maven.plugins</groupId>
+//<artifactId>maven-surefire-plugin</artifactId>
+//<version>3.0.0-M7</version>
+// <configuration>
+//<suiteXmlFiles>
+//  <suiteXmlFile>testng.xml</suiteXmlFile>
+//</suiteXmlFiles>
+//</configuration>
